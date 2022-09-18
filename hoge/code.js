8d42212223a83fd446c812aed2c11543386b358e
yuseirf <yusei.tanaka2112325@icloud.com>
2022-09-18 18:15:42 +0800

    check simple class and inheritance

--- hoge/code.js
+++ hoge/code.js
@@ -1,16 +1,44 @@
 "use strict";
-console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
-console.log(-Number.MAX_VALUE); // -1.7976931348623157e+308
-console.log(Number.MAX_VALUE + 1 == Number.MAX_VALUE); // true!
-console.log(-Number.MAX_VALUE - 1 == -Number.MAX_VALUE); // true!
-// 無限の限界値まで来ると足しても　true　になる
-console.log(Number.MAX_VALUE + 10 ** 1000); // Infinity
-console.log(-Number.MAX_VALUE - 10 ** 1000); // -Infinity
-// 特殊な値　Infinity　を返す
-console.log(1 / 0); // Infinity
-console.log(-1 / 0); // -Infinity
-// python　ではエラーが出るところもと特殊な値Infinityになる
-console.log(Infinity > 1); // true
-console.log(-Infinity < -1); // true
-// 正しく動く
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    add(point) {
+        return new Point(this.x + point.x, this.y + point.y);
+    }
+}
+var p1 = new Point(0, 10);
+var p2 = new Point(10, 20);
+var p3 = p1.add(p2); // {x:10,y:30}
+console.log(p1);
+console.log(p2);
+console.log(p3);
+class Point3D extends Point {
+    constructor(x, y, z) {
+        super(x, y);
+        this.z = z;
+    }
+    add(point) {
+        var point2D = super.add(point);
+        return new Point3D(point2D.x, point2D.y, this.z + point.z);
+    }
+}
+var p3d1 = new Point3D(p1.x, p1.y, 10);
+// Pointクラスからのでp1のxとyを継承
+console.log(p3d1);
+class StaticPoint3D extends Point {
+    constructor(x, y) {
+        super(x, y);
+        StaticPoint3D.z;
+    }
+    add(point) {
+        var point2D = super.add(point);
+        return new StaticPoint3D(point2D.x, point2D.y, StaticPoint3D.z);
+    }
+}
+StaticPoint3D.z = 100;
+// zはあらかじめ指定しているので引数に不要
+var staticp3d = new StaticPoint3D(30, 50);
+console.log(staticp3d);
 //# sourceMappingURL=code.js.map
--- hoge/code.ts
+++ hoge/code.ts
@@ -1,17 +1,35 @@
-console.log(Number.MAX_VALUE);  // 1.7976931348623157e+308
-console.log(-Number.MAX_VALUE); // -1.7976931348623157e+308
-console.log(Number.MAX_VALUE + 1 == Number.MAX_VALUE);   // true!
-console.log(-Number.MAX_VALUE - 1 == -Number.MAX_VALUE); // true!
-// 無限の限界値まで来ると足しても　true　になる
+class Point {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+    add(point: Point) {
+        return new Point(this.x + point.x, this.y + point.y);
+    }
+}
 
-console.log(Number.MAX_VALUE + 10**1000);  // Infinity
-console.log(-Number.MAX_VALUE - 10**1000); // -Infinity
-// 特殊な値　Infinity　を返す
+var p1 = new Point(0, 10);
+var p2 = new Point(10, 20);
+var p3 = p1.add(p2); // {x:10,y:30}
 
-console.log( 1 / 0); // Infinity
-console.log(-1 / 0); // -Infinity
-// python　ではエラーが出るところもと特殊な値Infinityになる
+console.log(p1);
+console.log(p2);
+console.log(p3);
 
-console.log( Infinity >  1); // true
-console.log(-Infinity < -1); // true
-// 正しく動く
+class Point3D extends Point {
+    z: number;
+    constructor(x: number, y: number, z: number) {
+        super(x, y);
+        this.z = z;
+    }
+    add(point: Point3D) {
+        var point2D = super.add(point);
+        return new Point3D(point2D.x, point2D.y, this.z + point.z);
+    }
+}
+
+var p3d1 = new Point3D(p1.x, p1.y, 10);
+// Pointクラスからのでp1のxとyを継承
+console.log(p3d1);
