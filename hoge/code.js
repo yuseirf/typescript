e53a12f941360eb9c705c1f24251f38ce58a0764
yuseirf <yusei.tanaka2112325@icloud.com>
2022-09-18 17:31:45 +0800

    check interesting features of Infinity

--- hoge/code.js
+++ hoge/code.js
@@ -1,8 +1,16 @@
 "use strict";
-console.log(Math.sqrt(-1)); // NaN
-// 平方根を出力する関数である sqrt() を使った時、負の数であるときは何も返さない
-// これはしないでください
-console.log(NaN === NaN); // false!!
-// こうしてください
-console.log(Number.isNaN(NaN)); // true
+console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+console.log(-Number.MAX_VALUE); // -1.7976931348623157e+308
+console.log(Number.MAX_VALUE + 1 == Number.MAX_VALUE); // true!
+console.log(-Number.MAX_VALUE - 1 == -Number.MAX_VALUE); // true!
+// 無限の限界値まで来ると足しても　true　になる
+console.log(Number.MAX_VALUE + 10 ** 1000); // Infinity
+console.log(-Number.MAX_VALUE - 10 ** 1000); // -Infinity
+// 特殊な値　Infinity　を返す
+console.log(1 / 0); // Infinity
+console.log(-1 / 0); // -Infinity
+// python　ではエラーが出るところもと特殊な値Infinityになる
+console.log(Infinity > 1); // true
+console.log(-Infinity < -1); // true
+// 正しく動く
 //# sourceMappingURL=code.js.map
