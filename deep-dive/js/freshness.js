d803ab5b7f5caec501276d4067afdbfe76630c4b
yuseirf <yusei.tanaka2112325@icloud.com>
2022-09-22 16:21:52 +0800

    check freshness

---
+++ deep-dive/js/freshness.js
@@ -0,0 +1,15 @@
+"use strict";
+function logIfHasName(something) {
+    if (something.name) {
+        console.log(something.name);
+    }
+}
+var person = { name: 'matt', job: 'being awesome' };
+var animal = { name: 'cow', diet: 'vegan, but has milk of own species' };
+logIfHasName(person); // okay
+logIfHasName(animal); // okay
+// Error: 型 '{ neme: string; }' の引数を型 '{ name?: string | undefined; }' のパラメーターに割り当てることはできません。
+// オブジェクト リテラルは既知のプロパティのみ指定できます。'neme' は型 '{ name?: string | undefined; }' に存在しません。
+// タイプミスを防止できる
+// logIfHasName({neme: 'I just misspelled name to neme'}); 
+//# sourceMappingURL=freshness.js.map
