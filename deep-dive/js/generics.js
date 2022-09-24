e477c3ff2b2a8fac0ecc68c6904d71b93635ce50
yuseirf <yusei.tanaka2112325@icloud.com>
2022-09-24 17:34:24 +0800

    check generics

--- deep-dive/js/generics.js
+++ deep-dive/js/generics.js
@@ -1,26 +1,22 @@
+"use strict";
 // class Queue {
 //     private data: number[] = [];
 //     push(item: number) { this.data.push(item); }
 //     pop() { return this.data.shift(); }
 // }
-
 // const queue = new Queue();
 // queue.push(0);
 // queue.push("1"); // Oops a mistake
-
 // // a developer walks into a bar
 // console.log(queue.pop().toPrecision(1)); // オブジェクトは 'undefined' である可能性があります。
 // console.log(queue.pop().toPrecision(1)); // オブジェクトは 'undefined' である可能性があります。
-
-class QueueNumber{
-    private data: number[] = [];
-    push(item: number) { this.data.push(item); }
+class QueueNumber {
+    constructor() {
+        this.data = [];
+    }
+    push(item) { this.data.push(item); }
     pop() { return this.data.shift(); }
 }
-
 const queue = new QueueNumber();
 queue.push(0);
-// queue.push("1"); // ERROR : cannot push a string. Only numbers allowed 型 'string' の引数を型 'number' のパラメーターに割り当てることはできません。
-// ^ if that error is fixed the rest would be fine too
-
-declare function send<T>(arg: T): void;
+//# sourceMappingURL=generics.js.map
