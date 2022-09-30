f3a8b33f7d08d65b02d53aa3557936ee5378cc74
yuseirf <yusei.tanaka2112325@icloud.com>
2022-09-30 17:09:51 +0800

    check type inference

--- deep-dive/js/type-inference.js
+++ deep-dive/js/type-inference.js
@@ -1,16 +1,16 @@
-type Adder = (a: number, b: number) => number;
-let fooByAdder: Adder = (a, b) => {
+"use strict";
+let fooByAdder = (a, b) => {
     a = 10;
     b = 20;
     return a + b;
-}
+};
 console.log(fooByAdder);
 // console.log(fooByAdder.a); プロパティ 'a' は型 'Adder' に存在しません。
-
 let destructuring = {
     destructuringA: 123,
     destructuringB: 456
 };
-let {destructuringA} = destructuring;
+let destructuringA = destructuring;
 // a = "hello"; // Would Error: cannot assign `string` to a `number`
 console.log(destructuringA);
+//# sourceMappingURL=type-inference.js.map
